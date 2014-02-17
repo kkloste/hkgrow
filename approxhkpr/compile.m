45614216b6dce1071d64a591fc0decc9e4d720b3
David Gleich <dgleich@purdue.edu>
2014-02-16 19:44:06 -0500

    Remove old code

--- approxhkpr/compile.m
+++
@@ -1,7 +0,0 @@
-
-if ismac
-mex -g -largeArrayDims approxhkpr_mex.cpp
-else
-mex -g -O CXXFLAGS="\$CXXFLAGS -std=c++0x" -largeArrayDims hkpr_mex.cpp
-mex -g -O CXXFLAGS="\$CXXFLAGS -std=c++0x" -largeArrayDims approxhkpr_mex.cpp
-end
